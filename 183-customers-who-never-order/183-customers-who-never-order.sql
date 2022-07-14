a82cb742695021c72dcad88e421e8aab0190b50f
Surbhi Sawan <66005159+surbhisawan@users.noreply.github.com>
2022-07-15 01:16:52 +0530

    Time: 402 ms (92.90%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -1,4 +1,4 @@
-select c.name as "Customers"
-from customers c left join orders o
-on c.id = o.customerId
-where o.customerId is null;
+select c.name as 'Customers'
+from Customers c left join orders o
+on c.id=o.customerId
+where o.customerid is null;
