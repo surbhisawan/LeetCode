afb5aefa9185a9d15c20292bef88660b881db897
Surbhi Sawan <66005159+surbhisawan@users.noreply.github.com>
2022-08-07 22:35:12 +0530

    Time: 34 ms (55.90%), Space: 23.3 MB (40.83%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -4,14 +4,7 @@ public:
     int n = s.size();    
     for(int i=0,j=n-1;i<j;i++,j--)
 	{
-		int t=s[i];
-		s[i]=s[j];
-		s[j]=t;
+		swap(s[i],s[j]);
 	}
-	for(int i=0;i<n;i++)
-	{
-		cout<<s[i]<<" ";
-	}
-       
     }
 };
