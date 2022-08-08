bfcc74d6f771381d99ac36a27824a53bf6922922
Surbhi Sawan <66005159+surbhisawan@users.noreply.github.com>
2022-08-08 21:07:23 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (60.10%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+     int count[26]={0}; 
+     for(int i=0;i<s.length();i++){
+         count[s[i]-'a']++;
+         if(count[s[i]-'a']>1)
+             return s[i];
+     }   
+        return 0;
+    }
+};
