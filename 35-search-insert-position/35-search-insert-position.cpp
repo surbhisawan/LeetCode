0160038525cc70821b38b0a3abf35301fe5984f0
Surbhi Sawan <66005159+surbhisawan@users.noreply.github.com>
2022-08-23 00:49:27 +0530

    Time: 7 ms (62.10%), Space: 9.7 MB (72.14%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -19,20 +19,6 @@ public:
         return ans;
         
         
-//         int ans=0;
-//         while(start<=last)
-//         {
-//             int mid=start+(last-start)/2;
-//             if(nums[mid]==target){
-//                 return mid;
-//             }
-//             else if(nums[mid]<target){
-//                 start=mid+1;
-//             }
-//             else{
-//                 last=mid-1;
-//             }
-//         }
-//         return start;
+
     }
 };
