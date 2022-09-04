f78927bb57e28630ac946561c760ecf8e2718c05
Surbhi Sawan <66005159+surbhisawan@users.noreply.github.com>
2022-09-04 16:03:59 +0530

    Time: 803 ms (39.63%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,8 +1,8 @@
 select employee_id, salary as bonus
-from employees 
-where employee_id%2!=0 and name not like 'M%'
+from employees
+where name not like 'm%' and employee_id%2=1
 union
 select employee_id, 0
 from employees
-where employee_id%2=0 or name like 'M%'
+where name like 'm%' or employee_id%2=0
 order by employee_id;
