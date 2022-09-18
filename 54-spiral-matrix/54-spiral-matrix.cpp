2104818f9321254ef88b99b11b3fe587f2d1e0c4
Surbhi Sawan <66005159+surbhisawan@users.noreply.github.com>
2022-09-18 12:02:15 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (72.01%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -4,43 +4,44 @@ public:
         int row=matrix.size();
         int col=matrix[0].size();
         
+        vector<int>ans;
+        
         int starting_row=0;
         int starting_col=0;
         int ending_row=row-1;
         int ending_col=col-1;
         
-        int total=row*col;
         int count=0;
+        int total=row*col;
+        while(count<total){
         
-        vector<int>ans;
+        for(int i=starting_col;count<total && i<=ending_col;i++){
+            ans.push_back(matrix[starting_row][i]);
+            count++;
+        }
+        starting_row++;
         
-        while(count<total)
-        {
-            for(int i=starting_col;count<total && i<=ending_col;i++){
-                ans.push_back(matrix[starting_row][i]);
-                count++;
-            }
-            starting_row++;
+        for(int i=starting_row;count<total && i<=ending_row;i++){
+            ans.push_back(matrix[i][ending_col]);
+            count++;
+        }
+        ending_col--;
+        
+        for(int i=ending_col;count<total && i>=starting_col;i--){
+            ans.push_back(matrix[ending_row][i]);
+            count++;
+        }
+        ending_row--;
+        
+        for(int i=ending_row;count<total && i>=starting_row;i--){
+            ans.push_back(matrix[i][starting_col]);
+            count++;
+        }
+        starting_col++; 
             
-            for(int i=starting_row;count<total && i<=ending_row;i++){
-                ans.push_back(matrix[i][ending_col]);
-                count++;
-            }
-            ending_col--;
-            
-            for(int i=ending_col;count<total && i>=starting_col;i--){
-                ans.push_back(matrix[ending_row][i]);
-                count++;
-            }
-            ending_row--;
-            
-            for(int i=ending_row;count<total && i>=starting_row;i--){
-                ans.push_back(matrix[i][starting_col]);
-                count++;
-            }
-            starting_col++;
             
         }
+        
         return ans;
     }
 };
