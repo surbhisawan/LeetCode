67539959472901febc987097f5d659dff023d815
Surbhi Sawan <66005159+surbhisawan@users.noreply.github.com>
2022-09-22 00:03:54 +0530

    Time: 11 ms (36.43%), Space: 7.7 MB (87.34%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& acc) {
-        int row = acc.size();
-        int col = acc[0].size();
-        int maxNo = INT_MIN;
+        int row=acc.size();
+        int col=acc[0].size();
+        
+        int maxWealth=INT_MIN;
+        
         for(int i=0;i<row;i++){
-            int sum = 0;
+            int sum=0;
             for(int j=0;j<col;j++){
                 sum+=acc[i][j];
             }
-            maxNo = max(maxNo,sum);
+            maxWealth = max(sum,maxWealth);
         }
-        return maxNo;
+        return maxWealth;
     }
 };
