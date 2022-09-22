1e6c391c6e4274cf67c1043b69e521c0cd6bee76
Surbhi Sawan <66005159+surbhisawan@users.noreply.github.com>
2022-09-22 09:30:46 +0530

    Time: 30 ms (31.20%), Space: 11.2 MB (58.60%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -3,14 +3,15 @@ public:
     int diagonalSum(vector<vector<int>>& matrix) {
         int row = matrix.size();
         int col = matrix[0].size();
+        
         int sum=0;
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
-                if(i==j || (i+j)==row-1){
+                if(i==j || (i+j)==(row-1)){
                     sum+=matrix[i][j];
                 }
             }
         }
-        return sum;
+       return sum;
     }
 };
