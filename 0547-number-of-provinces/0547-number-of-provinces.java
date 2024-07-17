0462ad5eab418b27851495fb4d7054c692aa3cf3
Surbhi Sawan <66005159+surbhisawan@users.noreply.github.com>
2024-07-18 00:09:06 +0530

    Time: 1 ms (91.71%), Space: 47.9 MB (22.29%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,43 +1,29 @@
 class Solution {
     
-    public static void dfs(int src, ArrayList<ArrayList<Integer>> adj , boolean[] visited){
-        visited[src] = true;
+    public void dfs(int i, int[][] isConnected, boolean[] vis){
+        vis[i] = true;
         
-        for(Integer it : adj.get(src)){
-            if(!visited[it]){
-                dfs(it, adj, visited);
+        for(int ptr = 0; ptr < isConnected.length; ptr++ ){
+            if(isConnected[i][ptr] == 1 && !vis[ptr]){
+                dfs(ptr,isConnected,vis);
             }
         }
-        
     }
     
     public int findCircleNum(int[][] isConnected) {
+        
+        int n = isConnected.length;
+        boolean[] vis = new boolean[n];
+        
         int cnt = 0;
-        int size = isConnected.length;
         
-        boolean[] visited = new boolean[size];
-        
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
-        
-        for(int i = 0; i < size; i++){
-            adj.add(new ArrayList<Integer>());
-        }
-        
-        for(int i = 0; i < size; i++){
-            for(int  j = 0; j < size; j++){
-                if(isConnected[i][j] == 1 & i!=j){
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
+        for(int i = 0; i < n; i++){
+                if(!vis[i]){
+                    cnt++;
+                    dfs(i,isConnected,vis);
                 }
-            }
         }
         
-        for(int i = 0 ; i < size; i++){
-            if(!visited[i]){
-                dfs(i, adj, visited);
-                cnt++;
-            }
-        }
         return cnt;
     }
 }
