847f8f56b64adf3bd146c4cada4a6e35d306f142
Surbhi Sawan <66005159+surbhisawan@users.noreply.github.com>
2024-07-18 15:56:36 +0530

    Time: 4 ms (48.73%), Space: 48.3 MB (99.32%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,42 +1,58 @@
+class Pair{
+    int src;
+    int dest;
+    
+    Pair(int src, int dest){
+        this.src = src;
+        this.dest = dest;
+    }
+}
 
 class Solution {
-    
-    
-    void dfs(int i, int j, char[][]grid){
-        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1'){
-            return ;
-        }
-        
+    // Function to find the number of islands.
+    public void solve(int i, int j, char[][] grid){
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(i,j));
         grid[i][j] = '0';
-        dfs(i - 1 , j, grid);
-        dfs(i + 1, j, grid);
-        dfs(i , j + 1, grid);
-        dfs(i , j - 1, grid);
-    }
         
+        
+        int[] row = {-1,0,1,0};
+        int[] col = {0,-1,0,1};
+        
+        while(!q.isEmpty()){
+            int r = q.peek().src;
+            int c = q.peek().dest;
+            
+            q.poll();
+            
+            for(int a = 0; a<4 ; a++){
+                    int nr = r + row[a];
+                    int nc = c + col[a];
+                    
+                    if(nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length &&
+                      grid[nr][nc] == '1'){
+                          grid[nr][nc] = '0';
+                          q.add(new Pair(nr,nc));
+                      }
+            }
+        }
+    }
     
     public int numIslands(char[][] grid) {
-        
-        if(grid == null || grid.length == 0)
-            return 0;
-        
+        // Code here
         int row = grid.length;
         int col = grid[0].length;
         
         int cnt = 0;
         
-        for(int i = 0; i < row; i++)
-        {
-            for(int j = 0; j < col; j++)
-            {
-            if(grid[i][j] == '1')
-            {
-                cnt++;
-                dfs(i, j, grid);
-            }
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(grid[i][j] == '1'){
+                    cnt++;
+                    solve(i, j, grid);
+                }
             }
         }
-        
         return cnt;
     }
 }
