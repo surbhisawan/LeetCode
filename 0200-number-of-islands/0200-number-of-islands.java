e8eb1a78af249da5646a7f9683ef68aae0f0d93e
Surbhi Sawan <66005159+surbhisawan@users.noreply.github.com>
2024-10-02 23:48:56 +0530

    Time: 3 ms (85.94%), Space: 49.1 MB (85.21%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,55 +1,28 @@
-class Pair{
-    int src;
-    int dest;
-    
-    Pair(int src, int dest){
-        this.src = src;
-        this.dest = dest;
-    }
-}
-
 class Solution {
-    // Function to find the number of islands.
-    public void solve(int i, int j, char[][] grid){
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(i,j));
+    
+    public void dfs(int i, int j, char[][] grid){
+        
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != '1' ){
+            return;
+        }
+        
         grid[i][j] = '0';
         
-        
-        int[] row = {-1,0,1,0};
-        int[] col = {0,-1,0,1};
-        
-        while(!q.isEmpty()){
-            int r = q.peek().src;
-            int c = q.peek().dest;
-            
-            q.poll();
-            
-            for(int a = 0; a<4 ; a++){
-                    int nr = r + row[a];
-                    int nc = c + col[a];
-                    
-                    if(nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length &&
-                      grid[nr][nc] == '1'){
-                          grid[nr][nc] = '0';
-                          q.add(new Pair(nr,nc));
-                      }
-            }
-        }
+        dfs(i+1, j, grid);
+        dfs(i, j+1, grid);
+        dfs(i-1, j, grid);
+        dfs(i, j-1, grid);
     }
     
     public int numIslands(char[][] grid) {
-        // Code here
-        int row = grid.length;
-        int col = grid[0].length;
         
         int cnt = 0;
         
-        for(int i = 0; i < row; i++){
-            for(int j = 0; j < col; j++){
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
                 if(grid[i][j] == '1'){
+                    dfs(i,j, grid);
                     cnt++;
-                    solve(i, j, grid);
                 }
             }
         }
