fa0357754d968b17a8ac446d78d8e800ae0ec007
Surbhi Sawan <66005159+surbhisawan@users.noreply.github.com>
2024-10-04 21:48:38 +0530

    Time: 8 ms (80.35%), Space: 59.8 MB (25.09%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,56 +1,53 @@
-class Pair{
-    int r;
-    int c;
-    
-    Pair(int r, int c){
-        this.r = r;
-        this.c = c;
-    }
-}
-
 class Solution {
+    
+    public void dfs(int i, int j, int[][] grid, int[][] vis, int[] delrow, int[] delcol){
+        vis[i][j] = 1;
+        
+        for(int it = 0; it < 4; it++){
+            int nr = delrow[it] + i;
+            int nc = delcol[it] + j;
+            
+            if(nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length &&
+              grid[nr][nc] == 1 && vis[nr][nc] == 0){
+                dfs(nr,nc,grid,vis,delrow,delcol);
+            }
+         }
+    }
     
     public int numEnclaves(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
         
-        Queue<Pair> q = new LinkedList<>();
-        
         int[][] vis = new int[n][m];
+        
+        int[] delrow = {-1,0,1,0};
+        int[] delcol = {0,-1,0,1};
+        
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(i == 0 || i == n-1 || j == 0 || j == m-1){
-                    if(grid[i][j] == 1){
-                        q.add(new Pair(i,j));
-                        vis[i][j] = 1;
-                    }
-                }
+            if(grid[i][0] == 1 && vis[i][0] == 0){
+                dfs(i,0,grid, vis, delrow, delcol);
+            }
+            
+            if(grid[i][m-1] == 1 && vis[i][m-1] == 0){
+                dfs(i,m-1,grid, vis, delrow, delcol);
             }
         }
         
-        int[] delRow = {-1,0,1,0};
-        int[] delCol = {0,-1,0,1};
-        
-        while(!q.isEmpty()){
-            int r = q.peek().r;
-            int c = q.peek().c;
-            q.remove();
+        for(int j = 0; j < m; j++){
+            if(grid[0][j] == 1 && vis[0][j] == 0){
+                dfs(0,j,grid, vis, delrow, delcol);
+            }
             
-            for(int i = 0; i < 4; i++){
-                int nr = delRow[i] + r;
-                int nc = delCol[i] + c;
-                
-                if(nr >= 0 && nr < n && nc >=0 && nc < m && grid[nr][nc] == 1 && vis[nr][nc] == 0){
-                    vis[nr][nc] = 1;
-                    q.add(new Pair(nr,nc));
-                }   
+            if(grid[n-1][j] == 1 && vis[n-1][j] == 0){
+                dfs(n-1,j,grid, vis, delrow, delcol);
             }
         }
         
         int cnt = 0;
+        
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
-                if(vis[i][j] == 0 && grid[i][j] == 1){
+                if(grid[i][j] == 1 && vis[i][j] == 0){
                     cnt++;
                 }
             }
