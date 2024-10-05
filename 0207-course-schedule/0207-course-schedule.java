ebaa995fcbce81b7f69eaae66d4338781207747f
Surbhi Sawan <66005159+surbhisawan@users.noreply.github.com>
2024-10-05 14:31:38 +0530

    Time: 7 ms (39.14%), Space: 46.2 MB (5.07%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        
+        for(int i = 0; i < numCourses; i++){
+            ans.add(new ArrayList<>());
+        }
+        
+        for(int i = 0; i < prerequisites.length; i++){
+            ans.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+        
+        int[] deg = new int[numCourses];
+        
+        for(int i = 0; i < numCourses; i++){
+            for(int it : ans.get(i)){
+                deg[it]++;
+            }
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        
+        for(int i = 0; i < numCourses; i++){
+            if(deg[i] == 0){
+                q.add(i);
+            }
+        }
+        
+        int[] top = new int[numCourses];
+        int k = 0;
+        
+        while(!q.isEmpty()){
+            int node = q.peek();
+            q.poll();
+            
+            top[k++] = node;
+            
+            for(int it : ans.get(node)){
+                deg[it]--;
+                
+                if(deg[it] == 0){
+                    q.add(it);
+                }
+            }
+        }
+        
+        return k == numCourses;
+        
+    }
+}
