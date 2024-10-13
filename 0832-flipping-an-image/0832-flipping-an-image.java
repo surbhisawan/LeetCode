eae0ee15605d2e81477a0b260b92fae2752be98b
Surbhi Sawan <66005159+surbhisawan@users.noreply.github.com>
2024-10-13 13:03:23 +0530

    Time: 0 ms (100.00%), Space: 44.7 MB (27.75%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.java
+++ 0832-flipping-an-image/0832-flipping-an-image.java
@@ -6,18 +6,18 @@ class Solution {
         int temp = 0;
         
         for(int i = 0; i < r; i++){
-            for(int j = 0; j < c/2; j++){
-                temp = image[i][j];
-                image[i][j] = image[i][c-1-j];
+            for(int j = 0; j < (c+1)/2; j++){
+                temp = image[i][j]^1;
+                image[i][j] = image[i][c-1-j]^1;
                 image[i][c-1-j] = temp;
             }
         }
         
-        for(int i = 0; i < r; i++){
-            for(int j = 0; j < c; j++){
-                image[i][j] = image[i][j] == 0 ? 1:0;
-            }
-        }
+        // for(int i = 0; i < r; i++){
+        //     for(int j = 0; j < c; j++){
+        //         image[i][j] ^= 1;
+        //     }
+        // }
         return image;
     }
 }
